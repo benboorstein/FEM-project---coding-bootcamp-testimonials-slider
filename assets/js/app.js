c8a1eab1ed48409ba8788d359b646b3452e7a2c5
Ben <benboorstein@gmail.com>
2022-07-17 09:37:59 -0400

    Minor changes. Project finished for now.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 const testimonials = [
     { // Tanya
-        quote: '“ I\’ve been interested in coding for a while but never taken the jump, until now. I couldn\’t recommend this course enough. I\’m now in the job of my dreams and so excited about the future. ”',
+        quote: '“ I\'ve been interested in coding for a while but never taken the jump, until now. I couldn\'t recommend this course enough. I\'m now in the job of my dreams and so excited about the future. ”',
         quoteAttrName: 'Tanya Sinclair',
         quoteAttrPosition: 'UX Engineer',
         pic: './assets/img/image-tanya.jpg',
         picAlt: 'Picture of Tanya'
     },
     { // John
-        quote: '“ If you want to lay the best foundation possible I\’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
+        quote: '“ If you want to lay the best foundation possible I\'d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
         quoteAttrName: 'John Tarkpor',
         quoteAttrPosition: 'Junior Front-end Developer',
         pic: './assets/img/image-john.jpg',
@@ -22,8 +22,8 @@ const testimonials = [
     }  
 ]
 
-// FYI: The way the below is written means this app would work even if there were, say, 20 objects in the testimonials array instead of just 2.
-let counter = 0
+// FYI: The way the below is written means this app would work even if there were, say, 20 objects in the testimonials array instead of just 3.
+let counter = 0 // *** the key to understanding how this program works is remembering that 'counter' (here in the global scope) gets updated each/every time 'counter' gets updated in the 'getTestimonial()' function! ***
 
 function getTestimonial(num) {
     counter += num
@@ -90,12 +90,3 @@ function goToFirstPage() {
 }
 
 */
-
-
-
-
-
-
-
-
-
